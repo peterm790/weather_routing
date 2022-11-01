6724a6ed69b4929d64cf5ebd26c970565fcd0d69
Peter <petermarsh790@gmail.com>
2022-11-01 14:54:44 +0200

    change to in-wake pruning and some optimisations

--- isochronal_weather_router.py
+++ isochronal_weather_router.py
@@ -48,37 +48,27 @@ class weather_router:
             self.point_validity = lsm.point_validity_arr
         else:
             self.point_validity = point_validity
+    
+    def get_min_dist_wp(self, isochrones):
+        dists = []
+        for lat,lon in isochrones[:,:2]:
+            dists.append(geopy.distance.great_circle((lat,lon), self.end_point).nm)
+        return min(dists)
 
+    def get_dist_wp(self, isochrones):
+        dists = []
+        for lat,lon in isochrones[:,:2]:
+            dists.append(geopy.distance.great_circle((lat,lon), self.end_point).nm)
+        return dists
 
-    def getDist_wp(self, lat, lon):
-        return geopy.distance.great_circle((lat,lon), self.end_point).nm
-
-
-    def getBearing_from_start(self, lat2,lon2):
-        pointA = self.start_point
-        pointB = (lat2,lon2)
+    def getBearing(self, pointA, pointB):
         lat1 = math.radians(pointA[0])
         lat2 = math.radians(pointB[0])
         diffLong = math.radians(pointB[1] - pointA[1])
         x = math.sin(diffLong) * math.cos(lat2)
         y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1) * math.cos(lat2) * math.cos(diffLong))
         initial_bearing = math.atan2(x, y)
-        initial_bearing = math.degrees(initial_bearing)
-        compass_bearing = (initial_bearing + 360) % 360
-        return compass_bearing
-
-    def getBearing_to_end(self, lat2,lon2):
-        pointA = self.end_point
-        pointB = (lat2,lon2)
-        lat1 = math.radians(pointA[0])
-        lat2 = math.radians(pointB[0])
-        diffLong = math.radians(pointB[1] - pointA[1])
-        x = math.sin(diffLong) * math.cos(lat2)
-        y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1) * math.cos(lat2) * math.cos(diffLong))
-        initial_bearing = math.atan2(x, y)
-        initial_bearing = math.degrees(initial_bearing)
-        compass_bearing = (initial_bearing + 360) % 360
-        return compass_bearing
+        return (math.degrees(initial_bearing) + 360) % 360
 
     def getTWA_from_heading(self, bearing,TWD):
         TWA = bearing - TWD
@@ -87,27 +77,61 @@ class weather_router:
     def myround(self, x, base=1):
         return base * round(x/base)
 
-    def get_possible(self,lat_init, lon_init, route, t):
+    def get_wake_lims(self, bearing_to_finish):
+        backbearing = ((bearing_to_finish - 180) + 360) % 360
+        upper = ((backbearing+45) + 360) % 360
+        lower  = ((backbearing-45) + 360) % 360
+        return (upper,lower)
+
+    def is_not_in_wake(self, wake_lims, bearing):
+        in_wake = True
+        upper,lower = wake_lims
+        if upper > lower:
+            if bearing <= upper:
+                if bearing >= lower:
+                    in_wake = False
+        else:
+            if bearing >= lower:
+                in_wake = False
+            if bearing <= upper:
+                in_wake = False
+        return in_wake
+    
+    def prune_slow(self, possible):
+        arr = np.array(possible, dtype=object)
+        keep = [True] * len(arr)
+        for i in range(len(possible)):
+            if keep[i] == True:
+                for j in range(len(arr)):
+                    if not i == j:
+                        if keep[j] == True:
+                            bearing = self.getBearing((arr[i][0], arr[i][1]), (arr[j][0], arr[j][1])) #inputting lat,lon of array i and j
+                            bool_ = self.is_not_in_wake(self.get_wake_lims(arr[i][-1]), bearing) #inputting bearing to finish of i into get_wake_lims and bearing to other point
+                            keep[j] = bool_       
+        return arr[keep]
+    
+
+    def get_possible(self,lat_init, lon_init, route, bearing_end, t):
         possible = []
         twd, tws = self.get_wind(t, lat_init, lon_init)
-        route.append('dummy')
-        for heading in range(0,360,5):
+        #route.append('dummy')
+        upper = int(bearing_end)+135
+        lower  = int(bearing_end) -135
+        for heading in range(lower,upper,5):
+            heading = ((int(heading) + 360) % 360)
             twa = self.getTWA_from_heading(heading, twd)
             speed = self.polar.getSpeed(tws,np.abs(twa))
-            end_point = geopy.distance.geodesic(nautical=speed*self.step).destination((lat_init,lon_init), heading)
+            end_point = geopy.distance.great_circle(nautical=speed*self.step).destination((lat_init,lon_init), heading)
             lat,lon = end_point.latitude, end_point.longitude
-            route = route[:-1]
+            #route = route[:-1]
             route.append((lat,lon))
             if self.point_validity(lat, lon):
-                dist_wp = self.getDist_wp(lat, lon)
-                if dist_wp <= self.dist_wp_init:
-                    bearing_start = self.myround(int(self.getBearing_from_start(lat,lon)))            
-                    possible.append([lat, lon, route, dist_wp, bearing_start])
+                bearing_end = self.getBearing((lat,lon), self.end_point)           
+                possible.append([lat, lon, route, bearing_end])
         return possible
     
     def route(self):
         lat, lon = self.start_point
-        self.dist_wp_init = self.getDist_wp(lat, lon)
         self.isochrones = []
         if not self.point_validity(lat,lon):
             print('start point error')
@@ -118,16 +142,11 @@ class weather_router:
                 for step,t in enumerate(self.time_steps):
                     print(step)
                     if step == 0:
-                        possible = self.get_possible(lat, lon, [self.start_point], t)
+                        bearing_end = self.getBearing((lat,lon), self.end_point)
+                        possible = self.get_possible(lat, lon, [self.start_point], bearing_end, t)
                     else:
-                        arr = np.array(possible, dtype=object)
-                        arr = arr[arr[:, -1].argsort()]
-                        split = np.split(arr, np.unique(arr[:, -1], return_index=True)[1][1:])
-                        keep = []
-                        for a in split:
-                            keep.append(a[np.argmin(a[:,-2])])
-                        self.isochrones.append(np.array(keep))
-                        dist_wp = np.min(self.isochrones[-1][:,-2]) 
+                        self.isochrones.append(self.prune_slow(possible))
+                        dist_wp = self.get_min_dist_wp(self.isochrones[-1])
                         if dist_wp > 30:
                             if step == len(self.time_steps)-1:
                                 print('out of time')
@@ -135,8 +154,8 @@ class weather_router:
                                 break
                             else:
                                 possible_at_t = []
-                                for lat, lon, route in list(self.isochrones[-1][:,:3]):
-                                    possible_at_t.append(self.get_possible(lat, lon, route, t))
+                                for lat, lon, route, bearing_end in list(self.isochrones[-1]):
+                                    possible_at_t.append(self.get_possible(lat, lon, route, bearing_end, t))
                         else:
                                 print('reached dest')
                                 not_done = False
@@ -147,7 +166,8 @@ class weather_router:
         return self.isochrones
 
     def get_fastest_route(self):
-        df = pd.DataFrame(np.concatenate(self.isochrones))
-        df .columns =  ['lat', 'lon','route', 'dist_wp','brg_start']
+        df = pd.DataFrame(self.isochrones[-1])
+        df.columns =  ['lat', 'lon','route', 'brg']
+        df['dist_wp'] = self.get_dist_wp(self.isochrones[-1])
         return df.iloc[pd.to_numeric(df['dist_wp']).idxmin()].route
 
